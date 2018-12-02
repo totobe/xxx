0bfcb2b9a8a3822a430d18807e924f0d0f641df5
Java <Java@DESKTOP-TQ00GH9>
2018-12-02 16:09:39 +0800

    v3

--- src/xxx/test.java
+++ src/xxx/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("Hello World");
-System.out.print("Hello World");
+System.out.print("Hello World2");
 	}
 
 }
